28ed81cecca82f32264d32b37e5297588b2dd37e
wilsaj <wilson.andrew.j+github@gmail.com>
2015-12-01 18:17:32 -0600

    add a stream to convert points to geojson

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var es = require('event-stream');
 var JSONStream = require('JSONStream');
 var R = require('ramda');
 var request = require('request');
+var point = require('turf-point')
 
 var MAP_POINTS_URL = 'http://water.weather.gov/ahps/get_map_points.php'
 
@@ -10,24 +11,8 @@ function stream(key, options) {
   if (key === undefined) {
     throw new Error ('a key is required - it can be a two-letter state code (ex: "tx") or an NWS office code (ex: "ewx")');
   }
-  var form = normalize(options);
 
-  return request.post(MAP_POINTS_URL)
-    .form({
-      key: key,
-      fcst_type: 'obs',
-      percent: 50,
-      current_type: 'all',
-      populate_viewport: 1,
-      timeframe: 0,
-    })
-    .pipe(JSONStream.parse())
-    .pipe(es.map(function (data, callback) {
-      callback(null, data.points);
-    }));
-};
-
-function normalize(options) {
+  // normalize options
   var defaults = {
     fcst_type: 'obs',
     percent: 50,
@@ -35,11 +20,34 @@ function normalize(options) {
     populate_viewport: 1,
     timeframe: 0,
   };
+  var form = R.merge(defaults, options);
+  form.key = key;
 
-  return R.merge(defaults, options);
+  return request.post(MAP_POINTS_URL)
+    .form(form)
+    .pipe(JSONStream.parse())
+    .pipe(es.through(function write(data) {
+      for (var i = 0, len = data.points.length; i < len; i++) {
+        this.emit('data', data.points[i]);
+      }
+    }));
 };
 
+function geojsonify(options) {
+  var throughStream = es.map(function(data, callback) {
+    var coordinates = [data.longitude, data.latitude];
+    var properties = R.omit(['latitude', 'longitude'], data);
+
+    var feature = point(coordinates, properties);
+    feature.id = properties.lid;
+
+    callback(null, feature);
+  });
+
+  return throughStream;
+}
 
 module.exports = {
   stream: stream,
+  geojsonify: geojsonify,
 }
